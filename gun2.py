a0a779dee3def0a8b865fdeaf7bb44a83e691bc8
farukakpinar52 <faruk.akpinar@hotmail.com>
2023-03-12 22:16:45 +0300

    dizilerin tanıtılması

---
+++ gun2.py
@@ -0,0 +1,15 @@
+#f-string ve kullanıcıdan veri alarak alınan verinin uygun formata dönüştürülmesi
+# vade = int(input("Vade sayısını giriniz : "))
+# krediTutar = int(input("Kredi tutarınızı giriniz : "))
+# sonucMesaj = f"Aylık kredi geri ödemeniz {int(krediTutar/vade)} TL'dir."
+# print(sonucMesaj)
+
+dizi =["a","b","c"]
+print(dizi)
+dizi.append("d")
+print(dizi)
+
+dizi.clear()
+print(dizi)
+
+
