249616fd84554e892514977f3ca3152d1b5e0ac5
farukakpinar52 <faruk.akpinar@hotmail.com>
2023-03-12 22:31:49 +0300

    dizi fonksiyonları

--- gun2.py
+++ gun2.py
@@ -6,10 +6,23 @@
 
 dizi =["a","b","c"]
 print(dizi)
-dizi.append("d")
+dizi.append("d") #dizinin sonuna eleman ekler
 print(dizi)
 
 dizi.clear()
 print(dizi)
+
+dizi2=["asd",2,True]
+dizi2.pop()  #dizinin son elemannı siler ve geri dönderir
+dizi2.pop(0)  #dizinin 0.index elemannı siler ve geri dönderir
+print(dizi2)
+
+dizi3=[231,11,5,111,"aaaa"]
+dizi3.remove(11) # pop fonk. index ile çalışırken remove fonksiyonu value yani değer ile çalışır
+
+dizi3.extend(["abim","ben","sen"]) #diziyi buradaki elemanları dizimizin sonuna ekleyerek genişlettik
+print(dizi3)
+
+
 
 
